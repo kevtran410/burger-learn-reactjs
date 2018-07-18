2262c9934c7ce0160cd3c6683cca60a1634ddef7
Tran Ngoc Nam <kevtran.410@gmail.com>
2018-07-18 11:01:48 +0700

    Add Sidedrawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,8 +2,8 @@ import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
-const Logo = () => (
-  <div className={classes.Logo}>
+const Logo = (props) => (
+  <div className={classes.Logo} style={{height: props.height}}>
     <img src={burgerLogo} alt="My Burger" />
   </div>
 );
